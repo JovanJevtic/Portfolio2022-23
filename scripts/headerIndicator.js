236024ab55c0a16baf1c8521bf1272c20bc98eca
Jovan Jevtic <jovanjevtic132@gmail.com>
2023-09-20 17:49:00 +0200

    header translate button

--- scripts/headerIndicator.js
+++ scripts/headerIndicator.js
@@ -1,13 +1,27 @@
 const  headerIndicatorFn = () => {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
     document.getElementById("progressed").style.width = (90 -  scrolled) + "%";
-    console.log((100 -  scrolled) + "%");
+}
+ 
+const translateOptions = () => {
+    const btn = document.getElementById("translate-icon-wrapp")
+    const options = document.getElementById("translate-options-wrapp")
+
+    btn.addEventListener(('click'), () => {
+        if (options.style.display === "flex") {
+            options.style.display = "none"
+        } else {
+            options.style.display = "flex"
+
+        }
+    })
 }
 
 const headerIndicator = () => {
     window.onscroll = function() {headerIndicatorFn()};
+    translateOptions()
 }
 
 export default headerIndicator;
